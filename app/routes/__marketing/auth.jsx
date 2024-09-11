65fdebe5b6873d51b59ca34315ed0ee4f60bfced
Espen Strand Rørvik <47737668+espensr@users.noreply.github.com>
2024-09-11 12:58:09 +0200

    User authentication (#3)
    * Add user data model
    
    * Add server side credentials validation
    
    * Create user on signup
    
    * Validate user and create session cookie on login
    
    * Extract session data and add logout functionality
    
    * Add correct redirectPaths
    
    * Add route protection
    
    * Connect expenses and users
    
    * Make logout work from expenses routes

--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -1,4 +1,6 @@
 import AuthForm from '~/components/auth/AuthForm'
+import { login, signup } from '~/data/auth.server'
+import { validateCredentials } from '~/data/validation.server'
 import authStyles from '~/styles/auth.css'
 
 export default function AuthPage() {
@@ -6,18 +8,28 @@ export default function AuthPage() {
 }
 
 export async function action({ request }) {
-  const searchParams = new URL(request.uri).searchParams
+  const searchParams = new URL(request.url).searchParams
   const authMode = searchParams.get('mode') || 'login'
 
   const formData = await request.formData()
   const credentials = Object.fromEntries(formData)
 
-  // validate user input
+  try {
+    validateCredentials(credentials)
+  } catch (error) {
+    return error
+  }
 
-  if (authMode === 'login') {
-    // login logic
-  } else {
-    // signup logic (create user)
+  try {
+    if (authMode === 'login') {
+      return await login(credentials)
+    } else {
+      return await signup(credentials)
+    }
+  } catch (error) {
+    if (error.status === 422) {
+      return { credentials: error.message }
+    }
   }
 }
 
